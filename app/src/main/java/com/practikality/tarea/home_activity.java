aba9a57c6097afcf8966143ba769add73308c645
Aryaman21 <aryaman.agrawal21@gmail.com>
2019-05-04 23:59:47 +0530

    home_bottom_appbar
    Adding Home Bottom Appbar

--- app/src/main/java/com/practikality/tarea/home_activity.java
+++ app/src/main/java/com/practikality/tarea/home_activity.java
@@ -1,37 +1,59 @@
 package com.practikality.tarea;
 
 import android.support.annotation.NonNull;
+import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 
 public class home_activity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
+    BottomAppBar bottomAppBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activity);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
-        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        floatingActionButton = findViewById(R.id.fab);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        setSupportActionBar(bottomAppBar);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.action_bar_todo:
-                        break;
-                    case R.id.action_bar_assigned:
-                        break;
-                    case R.id.action_bar_settings:
-                        break;
-                }
-                return false;
+            public void onClick(View v) {
+                //TODO: Add task function
             }
         });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_bar_todo:
+                break;
+            case R.id.action_bar_assigned:
+                break;
+            case R.id.action_bar_settings:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
